5518304dc416c6e9f4f9b5e7333591e93a9a8809
Christos <chrivasileiou2@gmail.com>
2024-03-18 09:50:21 -0500

    projects update

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['christos-vasileiou/atpgllm', 'christos-vasileiou/algorithms4logic-synth', 'christos-vasileiou/A-Sobel-filter-edge-detector-on-FPGA'], // List of repository names to display. example: ['christos-vasileiou/my-project1', 'christos-vasileiou/my-project2']
+        projects: ['christos-vasileiou/atpgllm', 'christos-vasileiou/logic-synth-ic', 'christos-vasileiou/sobel-filter-FPGA'], // List of repository names to display. example: ['christos-vasileiou/my-project1', 'christos-vasileiou/my-project2']
       },
     },
     external: {
